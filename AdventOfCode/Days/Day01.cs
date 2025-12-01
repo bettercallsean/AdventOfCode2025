c172e55c46846543a9e79104aa7a3b63c9e3fd29
Sean Edwards <hello@seanedwards.dev>
2025-12-01 18:12:30 +0000

    Solution for day 1

---
+++ AdventOfCode/Days/Day01.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode.Days;
+
+public class Day01 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var count = 0;
+        var value = 50;
+
+        foreach (var rotation in _input)
+        {
+            if (rotation[0] == 'L')
+            {
+                value -= int.Parse(rotation[1..]);
+            }
+            else
+            {
+                value += int.Parse(rotation[1..]);
+            }
+
+            if (value % 100 == 0)
+                count++;
+
+        }
+
+        return new($"{count}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var count = 0;
+        var value = 50;
+
+        foreach (var rotation in _input)
+        {
+            var increment = rotation[0] == 'L' ? 1 : -1;
+
+            for (var i = 0; i < int.Parse(rotation[1..]); i++)
+            {
+                value += increment;
+
+                if (value % 100 == 0)
+                    count++;
+            }
+
+        }
+
+        return new($"{count}");
+    }
+}
